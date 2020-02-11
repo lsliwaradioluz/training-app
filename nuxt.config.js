928adfb8107696131613424a3a2b613ac3eb0384
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-02-11 15:00:28 +0100

    add redirect middleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     title: 'Piti',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=3' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
