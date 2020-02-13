3f9767515e816ee99348d353442e3346a0f87cf4
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-02-13 22:33:34 +0100

    add mobile app meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,9 @@ export default {
     title: 'Piti',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=4' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }, 
-      { name: 'mobile-web-app-capable', content: 'yes' }
+      { name: 'mobile-web-app-capable', content: 'mobile-web-app-capable' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/android-chrome-192x192.png' }
