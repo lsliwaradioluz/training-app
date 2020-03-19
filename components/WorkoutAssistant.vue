caf28eb92e8dc5a01ef41f53f316b06133ba13e8
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-03-19 11:38:56 +0100

    try to fix voice assistant on safari (again)

--- components/WorkoutAssistant.vue
+++ components/WorkoutAssistant.vue
@@ -99,6 +99,7 @@
         <i class="flaticon-clock small" :class="{ 't-green': showStopwatch }" @click="showStopwatch = !showStopwatch"></i>
         <i class="flaticon-menu-1 small" :class="{ 'pulsing-element': current.exercise.name == 'Rozpoczynasz nowy blok' }" @click="showWholeComplex = true"></i>
       </div>
+      <p>{{ soundname }}</p>
       <audio 
         @playing="voiceAssistantSpeaking = true" 
         @ended="voiceAssistantSpeaking = false" 
