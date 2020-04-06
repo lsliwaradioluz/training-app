f524f986c65d01e5a1c38f99cdb275dd4b91e9f8
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-04-06 13:53:44 +0200

    create piti product website in the root index.vue file

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ export default {
       background_color:'#B0FE76',
       orientation: 'portrait',
       theme_color: '#343F68', 
-      start_url: 'https://piti.live/dashboard'
+      start_url: 'https://www.piti.live/dashboard'
     },
   },
   apollo: {  
