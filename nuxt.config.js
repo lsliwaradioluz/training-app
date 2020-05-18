c121a0a6d1fb1d12db6f19e2d9e3e037b4ed6d18
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-05-18 21:04:03 +0200

    update project dependancies

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes, viewport-fit=cover' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'Follow along workouts everyday, everywhere' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icon.png' },
