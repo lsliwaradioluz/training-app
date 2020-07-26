1015cedfc4718ce552ae65095bd186caa04a8020
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-07-26 14:54:15 +0200

    finish mobile version of piti website

--- pages/index.vue
+++ pages/index.vue
@@ -400,6 +400,7 @@ img {
 }
 
 .welcome {
+  min-height: -webkit-fill-available;
   min-height: 80vh;
   background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url("https://images.unsplash.com/photo-1580086229554-1c2a34000456?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1294&q=80");
